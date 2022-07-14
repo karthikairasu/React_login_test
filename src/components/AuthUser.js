504aed021d986d418ce4011326dc8e3930f22a2b
karthikairasu <karthikairasuk@gmail.com>
2022-07-14 09:37:30 +0530

    api

---
+++ src/components/AuthUser.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axois from 'axios';
+
+function AuthUser() {
+    const http = axois.create({
+        baseURL:"http://api.ansara.in/api/login",
+        headers:{
+            "Content-type" : "application/json"
+        }
+
+    });
+  return {
+    http
+}
+}
+
+export default AuthUser
--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthUser from './AuthUser';
 
 function Login() {
+  const {http} = AuthUser();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+ 
+  const  submitForm = () => {
+    // console.log(email +' '+ password);
+    http.post('/login',{email:email,password:password}).then((res) => {
+     console.log(res.data);
+    });
+  }
+
   return (
-    <div>Login Page</div>
+    <div className="row justify-content-center pt-5">
+        <div className="col-sm-6">
+          <div className="card p-4">
+            <div className="from-group">
+              <label>Email address: </label>
+              <input type="email" className="form-control" placeholder="Enter email" onChange={e=>setEmail(e.target.value)} id="email"/>
+            </div>
+            <div className="from-group mt-3">
+              <label>Password: </label>
+              <input type="password" className="form-control" placeholder="Enter password" onChange={e=>setPassword(e.target.value)} id="pwd"/>
+            </div>
+            <button type="button" onClick={submitForm} className="btn btn-primary mt-4">Login</button>
+          </div>
+        </div>
+    </div>
   )
 }
 
