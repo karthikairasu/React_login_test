dcd065225dd1814346b9d5f17e61ef370f7338c5
abs-karthikairasuk-ui <65330823+abs-karthikairasuk-ui@users.noreply.github.com>
2022-07-17 18:22:00 +0530

    s

---
+++ src/api/axios.js
@@ -0,0 +1,5 @@
+import axois from 'axios';
+
+export default axois.create({
+    baseURL: 'http://api.ansara.in/api/login'
+})
--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,11 @@
-import React, {useRef, useState, useEffect } from 'react';
+import React, {useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "./context/AuthProvider";
+
+import axios from './api/axios';
+const LOGIN_URL = '/auth';
 
 const Login = () => {
+   const { setAuth } = useContext(AuthContext);
    const userRef = useRef();
    const errRef = useRef();
 
@@ -19,10 +24,37 @@ const Login = () => {
 
    const handleSubmit = async (e) => {
       e.preventDefault();
-      console.log(user, pwd);
-      setUser('');
-      setPwd('');
-      setSuccess(true);
+
+       try{
+        const response = await axios.post(LOGIN_URL,
+          JSON.stringify({user, pwd}),
+          {
+            headers: { 'Content-Type' : 'application/json'},
+            withCredentials : true
+          }
+          );
+         // console.log(user, pwd);
+        console.log(JSON.stringify(response?.data));
+        //console.log(JSON.stringify(response?.data));
+        const accessToken = response?.data?.accessToken;
+        const roles = response?.data?.roles;
+        setAuth({ user, pwd, roles, accessToken });
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+
+       }catch (err){
+          if(!err?.response){
+             setErrMsg('No Server Response');
+          } else if (err.response?.status === 400){
+             setErrMsg('Missing Username or Password');
+          } else if (err.response?.status === 401){
+             setErrMsg('Unauthorized');
+          } else {
+             setErrMsg('Login Failed');
+          }
+          errRef.current.focus();
+       }
    }
 
   return (
