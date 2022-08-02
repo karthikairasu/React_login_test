948a2d814ed98852dff5379fe6208f379c6f0bf6
abs-karthikairasuk-ui <65330823+abs-karthikairasuk-ui@users.noreply.github.com>
2022-08-02 15:24:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,82 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import {Routes, Route, Link} from 'react-router-dom';
-import Home from './components/home';
-import Login from './components/login';
-import Dashboard from './components/dashboard';
-import React, { Component }  from 'react';
-
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
+import Dashboard from "./components/dashboard";
+import React, { Component } from "react";
 
 function App() {
+  let user = JSON.parse(localStorage.getItem("user-info"));
+  const navigate = useNavigate();
+  //console.warn(user)
+  function logOut() {
+    localStorage.clear();
+    navigate("/login");
+  }
   return (
     <div>
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
-          {/* <a class="navbar-brand" href="#">Logo</a> */}
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div className="container-fluid">
+          {/* <a className="navbar-brand" href="#">Logo</a> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
               </li>
-              <li class="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
               </li>
-              <li class="nav-item">
-              <Link className="nav-link" to="/dashboard">Dashboard</Link>
-              </li> 
+              <li className="nav-item">
+                <Link className="nav-link" to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
             </ul>
-            
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Dropdown</a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Link</a></li>
-                  <li><a class="dropdown-item" href="#">Another link</a></li>
-                  <li><a class="dropdown-item" href="#">A third link</a></li>
+
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  title=""
+                >
+                  User
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#" onClick={logOut}>
+                      Logout
+                    </a>
+                  </li>
                 </ul>
               </li>
-              </ul> 
+            </ul>
           </div>
         </div>
       </nav>
-       
-        <div className="container">
-          <Routes>
-             <Route path="/" element={<Home/>}/>
-             <Route path="/login" element={<Login/>}/>
-             <Route path="/dashboard" element={<Dashboard/>}/>
-          </Routes>
-        </div>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -7,43 +7,60 @@ function Login() {
 
   const navigate = useNavigate();
   useEffect(() => {
-    if (localStorage.getItem('user-info')){
+    if (localStorage.getItem("user-info")) {
       //navigate("/dashboard");
     }
-  }, [])
-  async function login(){
+  }, []);
+  async function login() {
     console.warn(email, password);
-    let item={email,password};
-    let result= await fetch("http://api.ansara.in/api/login",{
-      method: 'POST',
-      headers:{
-        "Content-Type":"application/json",
-        "Accept":"application/json"
+    let item = { email, password };
+    let result = await fetch("http://api.ansara.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-      body:JSON.stringify(item)
-    }); 
-    result = await result.json();
-    localStorage.setItem("user-info",JSON.stringify(result))
-    navigate("/dashboard")
-
+      body: JSON.stringify(item),
+    });
+    if (email === "" || password === "") {
+      navigate("/login");
+    } else {
+      result = await result.json();
+      localStorage.setItem("user-info", JSON.stringify(result));
+      navigate("/dashboard");
+    }
   }
   return (
     <div className="row justify-content-center pt-5">
-        <div className="col-sm-6">
-          <div className="card p-4">
-            <div className="from-group">
-              <label>Email address: </label>
-              <input type="email" className="form-control" placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)} id="email"/>
-            </div>
-            <div className="from-group mt-3">
-              <label>Password: </label>
-              <input type="password" className="form-control" placeholder="Enter password" onChange={(e)=>setPassword(e.target.value)} id="pwd"/>
-            </div>
-            <button onClick={login} className="btn btn-primary mt-4">Login</button>
+      <div className="col-sm-6">
+        <div className="card p-4">
+          <div className="from-group">
+            <label>Email address: </label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+            />
           </div>
+          <div className="from-group mt-3">
+            <label>Password: </label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+              id="pwd"
+            />
+          </div>
+          <button onClick={login} className="btn btn-primary mt-4">
+            Login
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
